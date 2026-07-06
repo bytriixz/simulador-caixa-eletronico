e0233b4d01a7d414bf2d16cf9d81d8d3bf0a0497
bytriixz <meirelesbeatriz14@gmail.com>
2026-07-06 20:24:00 -0300

    feat: funcionalidade de saque

--- caixa-eletronico.py
+++ caixa-eletronico.py
@@ -25,19 +25,79 @@ while status == 0:
         status = (int(input("Escolha uma opção: ")))
     
         
-        if status ==1:
+        if status ==1:  #Verificação de saldo
             print("Aguarde enquanto verificamos seu saldo.\n")
             time.sleep(3)
-            print(f"Seu saldo atual é de: {locale.currency(saldo, symbol=True)}\n")
+            print(f"Seu saldo atual é de: {locale.currency(saldo, symbol=True)}\n")#Formatação usando locale
             print("Retornando ao menu...")
             time.sleep(5)
             status +=-1
         
-        elif status ==2:
-            print("Esta função ainda não foi implementada.")
-            print("Retornado ao menu...")
+        elif status ==2: #Opção de saque
+            print("Encaminhando para opção de saque. Aguarde.\n")
             time.sleep(3)
-            status=0
+            #Informações adicionais
+            print("Para saques acima de R$ 5.000 é necessário entrar em contato com sua agência para solicitar o agendamento e a reserva do dinheiro.")
+            time.sleep(2)
+            print("---------------------------------------------------")
+            print("O caixa fornece notas de R$20, R$50, R$100 e R$200.")
+            print("---------------------------------------------------\n")
+            time.sleep(4)
+            
+            try:
+                quantia = float(input(f"Digite a quantia que deseja sacar (Valor mínimo R$20,00): "))
+                
+                #Condição para saldo insuficiente
+                if saldo < quantia : 
+                    print("Saldo insufiente")
+                    time.sleep(3)
+                    print("Retornando ao menu...")
+                    time.sleep(3)
+                    status+=-2
+                
+                #Condição para valor abaixo do mínimo
+                elif quantia <20:
+                    print("Valor inválido. O valor mínimo é de R$20.")
+                    time.sleep(3)
+                    print("Retornando ao menu...")
+                    time.sleep(3)
+                    status +=-2
+                
+                #Condição para valor acima de 5000
+                elif quantia > 5000:
+                    print("Para saques acima de R$ 5.000 é necessário entrar em contato com sua agência para solicitar o agendamento e a reserva do dinheiro.\n")
+                    print("Retornando ao menu... ")
+                    time.sleep(8)
+                    status=0
+                
+                #Verificação para notas disponíveis no caixa
+                elif quantia%20 == 0 or quantia%50 == 0 or quantia%100 == 0 or quantia%200 == 0:
+                    print(f"Valor escolhido: {locale.currency(quantia, symbol=True)}, aguarde a contagem das notas.\n")
+                    time.sleep(5)
+                    print("Retire as notas no contador.\n")
+                    time.sleep(3)
+                
+                    print(f"Saldo anterior: {locale.currency(saldo, symbol=True)}")
+                    saldo = saldo-quantia   
+                    print(f"Saldo atual: {locale.currency(saldo, symbol=True)}\n")
+                    time.sleep(4)
+                
+                    print("Retornando ao menu...")
+                    time.sleep(5)
+                    status +=-2
+            
+                #Condição para indisponibilidade de notas
+                else:
+                    print("Valor inválido. Digite apenas valores múltiplos das notas disponíveis.\n O caixa fornece notas de R$20, R$50, R$100 e R$200.")
+                    time.sleep(3)
+                    print("Retornando ao menu...")
+                    time.sleep(3)
+                    status +=-2
+                
+            except ValueError:
+                print("Entrada inválida. Digite apenas números")
+                time.sleep(3)
+                status = 0
             
         elif status ==3:
             print("Esta função ainda não foi implementada.")
